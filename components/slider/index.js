33338aa96a430bdbd201cdff48a5f31a4130d0ab
safayathussain <safayatofficial1@gmail.com>
2023-04-01 23:14:15 +0600

    first commit

---
+++ components/slider/index.js
@@ -0,0 +1,124 @@
+import Carousel from "nuka-carousel/lib/carousel";
+import React, { useEffect, useRef, useState } from "react";
+const Brands = () => {
+  const brandImages = [
+    ["Images/Brands/onnow.png", 'https://app.onnow.io/login/'],
+    ["Images/Brands/buet.png", 'https://hum.buet.ac.bd/'],
+    ["Images/Brands/du.png", 'https://criminology.du.ac.bd/'],
+    ["Images/Brands/tunibibi.png", 'https://tunibibi.com/'],
+    ["Images/Brands/poc.png", '/poc'],
+    ["Images/Brands/fuel.png", 'https://uftl.syscomatic.com/'],
+    ["Images/Brands/onnow.png", 'https://app.onnow.io/login/'],
+    ["Images/Brands/buet.png", 'https://hum.buet.ac.bd/'],
+    ["Images/Brands/du.png", 'https://criminology.du.ac.bd/'],
+    ["Images/Brands/tunibibi.png", 'https://tunibibi.com/'],
+    ["Images/Brands/poc.png", '/poc'],
+    ["Images/Brands/fuel.png", 'https://uftl.syscomatic.com/'],
+    
+  ];
+
+  const [screenWidth, setScreenWidth] = useState(null);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((counter) => counter + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenWidth(window.innerWidth);
+    }
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [screenWidth, counter]);
+  return (
+    <section class="mb-12 md:mb-24 xl:mb-36 bg-gray-700 h-screen flex justify-center items-center" id="services">
+      <div class="container mx-auto">
+        <div class="-mx-4 flex flex-wrap">
+          <div class="w-full px-4">
+            {/* <div class="flex flex-wrap items-center justify-between"> */}
+            <Carousel
+              // animation="fade"
+              autoplay
+              pauseOnHover
+              autoplayInterval={3000}
+              speed={500}
+              slidesToShow={screenWidth > 1024 ? 6 : screenWidth > 720 ? 4 : 2}
+              wrapAround={true}
+              renderBottomCenterControls={false}
+              renderCenterLeftControls={
+                brandImages.length > 6
+                  ? ({ previousSlide }) => (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-7 h-7 text-gray-500 font-bold cursor-pointer hover:text-gray-100"
+                        onClick={previousSlide}
+                        data-aos-delay={4000}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.75 19.5L8.25 12l7.5-7.5"
+                        />
+                      </svg>
+                    )
+                  : false
+              }
+              renderCenterRightControls={
+                brandImages.length > 6
+                  ? ({ nextSlide }) => (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-7 h-7 text-gray-500 font-bold cursor-pointer hover:text-gray-100"
+                        onClick={nextSlide}
+                        data-aos-delay={4000}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                        />
+                      </svg>
+                    )
+                  : false
+              }
+            >
+              {brandImages.map((item, index) => (
+                <a
+                  key={index}
+                  href={item[1]}
+                  class="flex w-[150px] items-center mx-auto h-full justify-center py-5 2xl:w-[180px]"
+                  data-aos-delay={50 + index * 100}
+                  data-aos="fade-up"
+                  target='blank'
+                >
+                  <img src={item[0]} alt="image" className="" />
+                </a>
+              ))}
+            </Carousel>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Brands;
